d98179396d7be5dc429e82d5a9e93f5ceb796991
Olusegun Mayungbe <o.mayungbe@oluadepefarms.com>
2022-06-28 12:00:26 +0000

    Added lis.h file

--- 0x00-python-hello_world/list.h
+++ 0x00-python-hello_world/list.h
@@ -1,25 +1,22 @@
-#ifndef LISTS_H
-#define LISTS_H
-
-#include <stdlib.h>
+#include "lists.h"
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * 
+ * check_cycle - check if list is cycle
+ * @list: type listint_t
+ * Return: always int
+ * case: 1 true, 0 false
  */
-typedef struct listint_s
+
+int check_cycle(listint_t *list)
 {
-    int n;
-    struct listint_s *next;
-} listint_t;
+listint_t *past = list, *futur = list;
 
-size_t print_listint(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int n);
-void free_listint(listint_t *head);
-int check_cycle(listint_t *list);
-
-#endif /* LISTS_H */
+while (past != NULL && futur != NULL && futur->next != NULL)
+{
+past = past->next;
+futur = futur->next->next;
+if (past == futur)
+return (1);
+}
+return (0);
+}
