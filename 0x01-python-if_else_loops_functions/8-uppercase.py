048e786728dbab9764732063650820f1a024a720
Olusegun Mayungbe <o.mayungbe@oluadepefarms.com>
2022-06-28 23:59:32 +0000

    Task 8 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,10 @@
-#!/usr/bin/env python3
-uppercase = __import__('8-uppercase').uppercase
-
-uppercase("best")
-uppercase("Best School 98 Battery street")
+#!/usr/bin/python3
+def uppercase(str):
+    case = 0
+    for i in str:
+        if ord(i) >= ord('a') and ord(i) <= ord('z'):
+            case = 32
+        else:
+            case = 0
+        print('{:c}'.format(ord(i) - case), end='')
+    print()
