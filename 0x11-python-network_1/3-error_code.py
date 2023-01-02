e0390a429a7bae1e10576b5d4e66369fad32c8e8
Olusegun mayungbe <o.mayungbe@oluadepefarms.com>
2023-01-02 19:46:46 +0100

    Python script takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8)

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 """
 takes:
-    - a URL 
-    - Sends a request to the URL 
-    - Displays the body of the response
+- a URL 
+- Sends a request to the URL 
+- Displays the body of the response
 """
 
 import sys
 from urllib.request import urlopen
 
 def main(url):
-    try:
-        with urlopen(url) as response:
-            body = response.read().decode('utf-8')
-            print(body)
-    except urllib.error.HTTPError as e:
-    print(f'Error code: {e.code}')
+try:
+with urlopen(url) as response:
+body = response.read().decode('utf-8')
+print(body)
+except urllib.error.HTTPError as e:
+print(f'Error code: {e.code}')
 
 if __name__ == '__main__':
-  main(sys.argv[1])
+main(sys.argv[1])
