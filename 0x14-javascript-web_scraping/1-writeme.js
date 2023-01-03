e5816868d3e377a33ffd1f62b2fc1034aa9d0767
Olusegun mayungbe <o.mayungbe@oluadepefarms.com>
2023-01-03 10:55:30 +0100

    Script writes a string to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-# Script that writes a string to a file
-
 const fs = require('fs');
-const args = process.argv;
-fs.writeFile(args[2], args[3], (err) => {
-  if (err) console.log(err);
+
+fs.writeFile(process.argv[2], process.argv[3], function (err) {
+  if (err) {
+    return (console.log(err));
+  }
 });
