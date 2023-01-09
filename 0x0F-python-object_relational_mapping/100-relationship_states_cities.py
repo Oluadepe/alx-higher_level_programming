1ab601063a8c9d61e4b042146a155c5dfff4eabb
Olusegun Mayungbe <o.mayungbe@oluadepefarms.com>
2023-01-09 16:53:24 +0000

    applied a fix

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,33 +1,26 @@
 #!/usr/bin/python3
-"""
-Creates the State "California" with the City "San Francisco" from a Database
-command-line args:
-    1st: mysql username
-    2nd: mysql password
-    3rd: Database
-"""
-from relationship_state import State, Base
-from relationship_city import City
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
-import sys
+"""adds the State object “California”
+with the City “San Francisco”
+to the database hbtn_0e_100_usa"""
 
+if __name__ == "__main__":
 
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3],
-                                  pool_pre_ping=True))
+    import sys
+    from relationship_state import Base, State
+    from relationship_city import City
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from sqlalchemy.schema import Table
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    new_state = State(name='California')
-    new_city = City(name='San Francisco', states=california)
-# alternative way to add city to state below
-#    new_state.cities.append(new_city)
-
-    session.add(new_state)
-    session.add(new_city)
+    session = Session(engine)
+    new_city = City(name='San Francisco')
+    new = State(name='California')
+    new.cities.append(new_city)
+    session.add_all([new, new_city])
     session.commit()
     session.close()
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-"""
-Contains State class and Base which is an instance of declarative_base()
-"""
-from sqlalchemy import Column, Integer, String, MetaData
+"""Definition of the State class with relationship to City"""
+
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-
-
-mymetadata = MetaData()
-Base = declarative_base(metadata=mymetadata)
+from relationship_city import City, Base
 
 
 class State(Base):
-    """
-    Class with id and name attributes of each state
-    """
+    """Class State"""
+
     __tablename__ = 'states'
-    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    id = Column(Integer, autoincrement=True,
+                primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="states")
+    cities = relationship('City', backref='state',
+                          cascade='all, delete-orphan')
